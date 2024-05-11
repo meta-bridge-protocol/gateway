b2fb363a6c0d60fa4b10cdde08ab5820163ff4d4
Hassan Abbasi <hassan.abbp@gmail.com>
2024-05-11 16:51:52 +0330

    Refactor deploy.ts | Add optional verification for deployAxelarGateway and deployEscrow functions

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,37 +1,52 @@
-import hre, { ethers, upgrades } from "hardhat"
+import hre, {ethers, upgrades} from "hardhat"
 
-async function deployAxelarGateway(admin: string, axlToken: string, realToken: string, deployer: string) {
-	let factory = await ethers.getContractFactory("AxelarGateway", {
-		signer: await ethers.getSigner(deployer)
-	})
-	let axelarGateway = await factory.deploy(admin, axlToken, realToken)
-	await axelarGateway.waitForDeployment()
-	try {
-		await hre.run("verify:verify", {
-			address: await axelarGateway.getAddress(),
-			constructorArguments: [admin, axlToken, realToken]
-		})
-	} catch {
-		console.log("Failed to verify")
-	}
-	return axelarGateway
+async function deployAxelarGateway(
+    admin: string,
+    axlToken: string,
+    realToken: string,
+    deployer: string,
+    verify: boolean = true
+) {
+    let factory = await ethers.getContractFactory("AxelarGateway", {
+        signer: await ethers.getSigner(deployer)
+    })
+    let axelarGateway = await factory.deploy(admin, axlToken, realToken)
+    await axelarGateway.waitForDeployment()
+    if (verify)
+        try {
+            await hre.run("verify:verify", {
+                address: await axelarGateway.getAddress(),
+                constructorArguments: [admin, axlToken, realToken]
+            })
+        } catch {
+            console.log("Failed to verify")
+        }
+    return axelarGateway
 }
 
-async function deployEscrow(gateway: string, deus: string, msig: string, thresholdAmount: string, deployer: string) {
-	const factory = await ethers.getContractFactory("Escrow", {
-		signer: await ethers.getSigner(deployer)
-	})
-	const escrow = await upgrades.deployProxy(factory, [gateway, deus, msig, thresholdAmount])
-	await escrow.waitForDeployment()
-	try {
-		await hre.run("verify:verify", {
-			address: await escrow.getAddress(),
-			constructorArguments: [gateway, deus, msig, thresholdAmount]
-		})
-	} catch {
-		console.log("Failed to verify")
-	}
-	return escrow
+async function deployEscrow(
+    gateway: string,
+    deus: string,
+    msig: string,
+    thresholdAmount: string,
+    deployer: string,
+    verify: boolean = true
+) {
+    const factory = await ethers.getContractFactory("Escrow", {
+        signer: await ethers.getSigner(deployer)
+    })
+    const escrow = await upgrades.deployProxy(factory, [gateway, deus, msig, thresholdAmount])
+    await escrow.waitForDeployment()
+    if (verify)
+        try {
+            await hre.run("verify:verify", {
+                address: await escrow.getAddress(),
+                constructorArguments: [gateway, deus, msig, thresholdAmount]
+            })
+        } catch {
+            console.log("Failed to verify")
+        }
+    return escrow
 }
 
-export { deployAxelarGateway, deployEscrow }
+export {deployAxelarGateway, deployEscrow}
